e9f521f66a4f795b1fc9f67cfcda5d512e56558b
Lucas Santos André <54511428+lucassantosandre@users.noreply.github.com>
2024-04-23 15:51:25 -0300

    added outputs

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -12,7 +12,9 @@ function run() {
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-  core.notice('Helo from my custom Js action!');
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput('website-url', websiteUrl); // ::set-output
+
 }
 
 run();
