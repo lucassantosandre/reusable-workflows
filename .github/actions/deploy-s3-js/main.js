a75f0935073df4f10d2f9a45e479d4f84aa03d05
Lucas Santos André <54511428+lucassantosandre@users.noreply.github.com>
2024-04-23 15:12:16 -0300

    added s3 deploy custom js action

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -3,6 +3,15 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // 1) Get some input values
+  const bucket = core.getInput('bucket-name', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2) Upload the files to S3
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
   core.notice('Helo from my custom Js action!');
 }
 
